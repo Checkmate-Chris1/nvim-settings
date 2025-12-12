7dd62040c92ecc2c2dfa177f764af9e2ff1e41ef
Christopher Walden <checkdoesgames@gmail.com>
2025-12-12 00:35:17 -0800

    Added ifnamemain Python snippet

--- lua/snippets/python.lua
+++ lua/snippets/python.lua
@@ -5,4 +5,24 @@ local i = require('luasnip').insert_node
 local rep = require('luasnip.extras').rep
 local fmt = require('luasnip.extras.fmt').fmt
 
-return {}
+local function if_name_main()
+  return {
+    t {
+      'def main():',
+      '\t',
+    },
+    i(0, 'pass'),
+    t {
+      '',
+      '',
+      '',
+      "if __name__ == '__main__':",
+      '\tmain()',
+    },
+  }
+end
+
+return {
+  --s('ifmain', if_name_main()),
+  s('main', if_name_main()),
+}
